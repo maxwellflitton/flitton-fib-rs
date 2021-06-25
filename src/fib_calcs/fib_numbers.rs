a7a9ed32aca558f9423fc94424ca55bf521b288c
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-06-26 00:53:54 +0100

    adding tests

--- src/fib_calcs/fib_numbers.rs
+++ src/fib_calcs/fib_numbers.rs
@@ -14,3 +14,16 @@ pub fn fibonacci_numbers(numbers: Vec<i32>) -> Vec<u64> {
     }
     return vec
 }
+
+
+#[cfg(test)]
+mod fibonacci_numbers_tests {
+
+	use super::fibonacci_numbers;
+
+	#[test]
+	fn test_run() {
+		let outcome = fibonacci_numbers([1, 2, 3, 4].to_vec());
+        assert_eq!(outcome, [1, 1, 2, 3]);
+	}
+}

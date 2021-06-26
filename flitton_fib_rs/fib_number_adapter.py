1593ae47b97c063e4a4bec6c7b47b6163fdd1a33
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-06-26 15:30:25 +0100

    adding adapter

---
+++ flitton_fib_rs/fib_number_adapter.py
@@ -0,0 +1,22 @@
+from typing import Union, List, Optional
+
+from .flitton_fib_rs import fibonacci_number, fibonacci_numbers
+
+
+class FlittonFibNumberAdapter:
+
+    def __init__(self, number_input: Union[int, List[int]]) -> None:
+        self.number_input: Union[int, List[int]] = number_input
+        self.success: bool = False
+        self.result: Optional[Union[int, List[int]]] = None
+        self.error_message: Optional[str] = None
+
+    def _process_input(self) -> None:
+        if isinstance(self.number_input, int):
+            self.result = fibonacci_number(n=self.number_input)
+            self.success = True
+        elif isinstance(self.number_input, list):
+            self.result = fibonacci_numbers(numbers=self.number_input)
+            self.success = True
+        else:
+            self.error_message = "input needs to be a list of ints or an int"

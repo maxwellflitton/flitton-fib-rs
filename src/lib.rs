1a9dfe126944eeade91c58219b588c3b9c529ce2
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-06-26 14:37:43 +0100

    adding basic function

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,16 @@ use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 
 
+#[pyfunction]
+fn say_hello() {
+    println!("saying hello from Rust!");
+}
+
+
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     // m.add_function(wrap_pyfunction!(fib_number, m)?)?;
+    m.add_wrapped(wrap_pyfunction!(say_hello));
     m.add_wrapped(wrap_pyfunction!(fibonacci_number));
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     // m.add_wrapped(wrap_pyfunction!(fibonacci_reccursive));

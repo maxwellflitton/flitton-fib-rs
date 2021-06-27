877f707ed459ca3a7e05a47173e06bca160c110b
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2021-06-27 16:44:11 +0100

    adding singleton

--- flitton_fib_rs/fib_number_adapter.py
+++ flitton_fib_rs/fib_number_adapter.py
@@ -1,6 +1,7 @@
 from typing import Union, List, Optional
 
 from .flitton_fib_rs import fibonacci_number, fibonacci_numbers
+from .counter import Counter
 
 
 class FlittonFibNumberAdapter:
@@ -11,13 +12,19 @@ class FlittonFibNumberAdapter:
         self.result: Optional[Union[int, List[int]]] = None
         self.error_message: Optional[str] = None
         self._process_input()
+        self._counter: Counter = Counter()
+
+    def _define_success(self) -> None:
+        self.success = True
+        self._counter.increase_count()
 
     def _process_input(self) -> None:
         if isinstance(self.number_input, int):
             self.result = fibonacci_number(n=self.number_input)
-            self.success = True
+            self._define_success()
+
         elif isinstance(self.number_input, list):
             self.result = fibonacci_numbers(numbers=self.number_input)
-            self.success = True
+            self._define_success()
         else:
             self.error_message = "input needs to be a list of ints or an int"
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::types::PyDict;
-use pyo3::exceptions::PyTypeError;
+// use pyo3::types::PyDict;
+// use pyo3::exceptions::PyTypeError;
 
 mod fib_calcs;
 
-use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
-use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+use 
+fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use 
+fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 
 
 #[pyfunction]

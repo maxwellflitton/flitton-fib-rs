8cbd800a06e6c7ee36d741a24243ae9552d6c5a8
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-07-09 01:10:46 +0100

    adding basic interface

--- src/interface/config.rs
+++ src/interface/config.rs
@@ -1,15 +1,27 @@
 use pyo3::prelude::{pyfunction, PyResult};
-use pyo3::types::PyDict;
+use pyo3::types::{PyDict, PyList};
 
+use crate::fib_calcs::fib_number::fibonacci_number;
 use crate::fib_calcs::{fib_number, fib_numbers};
 
-// use fib_calcs::{fib_number, fib_numbers};
+
+fn process_number(input_numbers: Vec<i32>) -> Vec<u64> {
+    let mut buffer: Vec<u64> = Vec::new();
+
+    for i in input_numbers {
+        buffer.push(fibonacci_number(i));
+    }
+    return buffer
+}
+
 
 
 #[pyfunction]
 pub fn run_config<'a>(config: &'a PyDict) -> PyResult<&'a PyDict> {
     match config.get_item("number") {
-        Some(data) => println!("here is the data {:?}", data),
+        Some(data) => {
+            config.set_item("NUMBER RESULTS", data);
+        },
         None => println!("number is not in the config")
     }
     match config.get_item("numbers") {

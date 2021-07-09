3c058e17522431994f92e62ce3e80729453ae300
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-07-09 01:22:46 +0100

    adding basic interface

--- src/interface/config.rs
+++ src/interface/config.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::{pyfunction, PyResult};
 use pyo3::types::{PyDict, PyList};
 
 use crate::fib_calcs::fib_number::fibonacci_number;
-use crate::fib_calcs::{fib_number, fib_numbers};
+use crate::fib_calcs::fib_numbers::fibonacci_numbers;
 
 
 fn process_number(input_numbers: Vec<i32>) -> Vec<u64> {
@@ -15,17 +15,31 @@ fn process_number(input_numbers: Vec<i32>) -> Vec<u64> {
 }
 
 
+fn process_numbers(input_numbers: Vec<Vec<i32>>) -> Vec<Vec<u64>> {
+    let mut buffer: Vec<Vec<u64>> = Vec::new();
+    for i in input_numbers {
+        buffer.push(fibonacci_numbers(i));
+    }
+    return buffer
+}
+
 
 #[pyfunction]
 pub fn run_config<'a>(config: &'a PyDict) -> PyResult<&'a PyDict> {
     match config.get_item("number") {
         Some(data) => {
-            config.set_item("NUMBER RESULTS", data);
+            match data.downcast(Vec<i32>) {
+                Ok(unwrapped_data) => config.set_item("NUMBER RESULTS", 
+                process_number(unwrapped_data)),
+                Err(_) => println!("number is not a list of integers")
+            }
         },
         None => println!("number is not in the config")
     }
     match config.get_item("numbers") {
-        Some(data) => println!("here is the data {:?}", data),
+        Some(data) => {
+            config.set_item("NUMBERS RESULTS", data);
+        },
         None => println!("number is not in the config")
     }
     return Ok(config)

e3857db3b9cce8be8b65efa7bcd9f20c3e524812
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2021-07-11 18:33:45 +0100

    setting up config

--- src/interface/config.rs
+++ src/interface/config.rs
@@ -42,7 +42,6 @@ pub fn run_config<'a>(config: &'a PyDict) -> PyResult<&'a PyDict> {
             match data.downcast::<PyList>() {
                 Ok(raw_data) => {
                     let processed_results_two: Vec<Vec<i32>> = raw_data.extract::<Vec<Vec<i32>>>().unwrap();
-                    // let fib_numbers: Vec<Vec<u64>> = processed_results_two.iter().map(|x| fibonacci_numbers(*x)).collect();
                     config.set_item("NUMBERS RESULT", process_numbers(processed_results_two));
                 },
                 Err(_) => Err(PyTypeError::new_err("parameter numbers is not a list of lists of integers")).unwrap()

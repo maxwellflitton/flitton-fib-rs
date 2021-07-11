438c2c57e29200d61f38aab69c94f07ef356bd05
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2021-07-11 20:55:50 +0100

    adding object

--- src/interface/object.rs
+++ src/interface/object.rs
@@ -1,4 +1,4 @@
-use pyo3::prelude::{pyfunction, PyResult};
+use pyo3::prelude::{pyfunction, PyResult, Python};
 use pyo3::types::{PyAny, PyList};
 use pyo3::exceptions::PyTypeError;
 
@@ -8,8 +8,13 @@ use crate::fib_calcs::fib_numbers::fibonacci_numbers;
 
 #[pyfunction]
 pub fn object_interface<'a>(input_object: &'a PyAny) -> PyResult<&'a PyAny> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+
     match input_object.getattr("number") {
-        Ok(_) => println!("the object has number"),
+        Ok(_) => {
+            input_object.setattr("number", PyList::new(py,&[1, 2, 3, 4, 5])).unwrap();
+        },
         Err(_) => println!("the object does not have number")
     }
     return Ok(input_object)

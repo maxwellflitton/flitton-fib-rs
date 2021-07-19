01408e3fb217cad6756a00764abe12e5ceef6480
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-07-19 03:27:31 +0100

    adding fully fledged object

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-// use pyo3::types::PyDict;
-// use pyo3::exceptions::PyTypeError;
+use std::time::{Duration, Instant};
+
 
 mod fib_calcs;
 mod interface;
@@ -20,6 +20,31 @@ fn say_hello() {
     println!("saying hello from Rust!");
 }
 
+fn time_add_vectors(total_vector_size: i32) -> u64 {
+    let now = Instant::now();
+    let total_vector_size: i32 = 10;
+    
+    let mut buffer: Vec<i32> = Vec::new();
+    let first_vector: Vec<i32> = (0..total_vector_size.clone()).map(|x| x).collect();
+    let second_vector: Vec<i32> = (0..total_vector_size).map(|x| x).collect();
+    
+    for i in &first_vector {
+        buffer.push(first_vector[**&i as usize] + second_vector[*i as usize]);
+    }
+   return now.elapsed().as_secs()
+}
+
+#[pyfunction]
+fn process_range(total: i32) -> Vec<u64> {
+    let mut buffer: Vec<u64> = Vec::new();
+    let total_jobs: Vec<i32> = (0..total).map(|x| x).collect();
+
+    for i in total_jobs {
+        buffer.push(time_add_vectors(i));
+    }
+    return buffer
+}
+
 
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -29,6 +54,7 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
+    m.add_wrapped(wrap_pyfunction!(process_range));
     m.add_class::<FibProcessor>()?;
     // m.add_wrapped(wrap_pyfunction!(fibonacci_reccursive));
     // m.add_wrapped(wrap_pyfunction!(internal_fib_number));

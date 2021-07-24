6e2b0cdf898b03eb9121aec15c222c4063b556b4
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-07-24 15:29:57 +0100

    adding fully fledged object

--- flitton_fib_rs/numpy_interface.py
+++ flitton_fib_rs/numpy_interface.py
@@ -8,10 +8,10 @@ class NumpyInterface:
     
     def calc_times(self, distance, traffic_grade):
         result = calculate_times({}, distance, traffic_grade)
-        self.inventory["car time"] = result[0][0]
-        self.inventory["truck time"] = result[1][0]
+        self.inventory["car time"] = result["times"][0][0]
+        self.inventory["truck time"] = result["times"][1][0]
         
     def calc_parameters(self, car_time, truck_time):
         result = calculate_parameters({}, car_time, truck_time)
-        self.inventory["distance"] = result[0][0]
-        self.inventory["traffic grade"] = result[1][0]
+        self.inventory["distance"] = result["parameters"][0][0]
+        self.inventory["traffic grade"] = result["parameters"][1][0]
